b31db5f9fb1585d93c0b8be6bd4fbc33b5bf0300
aapontev <tu-email@ejemplo.com>
2026-02-03 21:14:36 -0500

    Modificaciones1

--- app/components/SchemaMarkup.tsx
+++ app/components/SchemaMarkup.tsx
@@ -60,7 +60,7 @@ export default function SchemaMarkup() {
                                 "@type": "ListItem",
                                 "position": 2,
                                 "name": "Calculadora de Luz Perú",
-                                "item": "https://www.gestorluz.com/CalculadoraLuz"
+                                "item": "https://www.gestorluz.com/calculadora-luz"
                             }
                         ]
                     },
